d48e9a5a849553a8c2e90ada47991ec054434c9d
William-0907 <williamsotnac@gmail.com>
2025-05-19 10:08:40 +0800

    final

--- final-project/login/loginapp/urls.py
+++ final-project/login/loginapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import login_view,users_view,stats_view,logout_view
+from .views import login_view,users_view,stats_view,logout_view,graph_view
 
 urlpatterns = [
     path('', login_view,name='login'),
     path('users', users_view,name='users',),
     path('stats', stats_view,name='stats'),
+    path('graph', graph_view,name='graph'),
     path('logout', logout_view,name='logout'),
 ]
--- final-project/login/loginapp/views.py
+++ final-project/login/loginapp/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth import login,logout,authenticate,get_user_model 
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from .models import Scores
 
 # Create your views here.
 def login_view(request):
@@ -34,6 +35,12 @@ def stats_view(request):
     total_users = User.objects.count()
     return render(request,'loginapp/user_stats.html', {'total_users':total_users})
 
+@login_required
+def graph_view(request):
+    scores = Scores.objects.all()
+    score_data = [score.score for score in scores]
+    users = [score.user.username for score in scores]
+    return render(request,'loginapp/graphs.html', {'score_data':score_data, 'users':users})
 
 @login_required
 def logout_view(request):
